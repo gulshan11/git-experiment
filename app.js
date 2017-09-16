43feead8bf5482f526dbf8c90651159b9f34a636
gulshan11 <gulshansavanth@gmail.com>
2017-09-16 18:10:08 +0530

    new-feature completed!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function aBigNewFeature(){
 	console.log('Writing a lot of code.');
 	console.log('Half way done with this new feature.');
+	console.log('Completely done with this new feature.');
 
 };
 function something() {
