2790c496074263e93c7b6dd32314f59f7ea502a5
gulshan11 <gulshansavanth@gmail.com>
2017-09-16 17:48:22 +0530

    Halfway done with this new feature.Tested so far, so good.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+function aBigNewFeature(){
+	console.log('Writing a lot of code.');
+	console.log('Half way done with this new feature.');
+
+};
 function something() {
 console.log('Printing 2nd line');
 }
 console.log('hello-world');
 something();
+aBigNewFeature();
